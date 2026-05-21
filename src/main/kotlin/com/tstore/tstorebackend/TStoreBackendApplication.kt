1d5c26ce8b714b2de045bc07ca04bf8b59af9926
Panha <thopanha123@gmail.com>
2026-05-21 17:06:47 +0700

    setup project

---
+++ src/main/kotlin/com/tstore/tstorebackend/TStoreBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.tstore.tstorebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TStoreBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<TStoreBackendApplication>(*args)
+}
